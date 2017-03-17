2b4f80e9f3977ee40d7d0ac2034c8ab0e07a1665
Tiago Oliveira <tiago@econtabil.com>
2017-03-16 21:30:11 -0300

    Exercicio 1 - Encontrar números multiplos de 3 e 7

--- src/com/opet/main.java
+++ src/com/opet/main.java
@@ -1,12 +1,23 @@
 package com.opet;
 
+import com.opet.util.Reader;
 public class main
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         // TODO Auto-generated method stub
+        System.out.println("Digite um número: ");
+        int numero = Reader.readInt();
+
+        for(int x = 1; x<= numero; x++) {
+            if( (x % 3 == 0) && (x % 7 == 0) ) {
+                System.out.println("Número "+x+" é múltiplo de 3 e 7");
+            }
+        }
 
     }
+
+
 
 }
