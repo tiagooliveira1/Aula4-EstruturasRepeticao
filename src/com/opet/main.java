141cc30cbabc7c05bb678ced21fa96bf2e3f6db8
Tiago Oliveira <tiago@econtabil.com>
2017-03-16 21:38:21 -0300

    Inserção do segundo exercício passado pelo professor. Verificar se é par

--- src/com/opet/main.java
+++ src/com/opet/main.java
@@ -10,10 +10,21 @@ public class main
         System.out.println("Digite um número: ");
         int numero = Reader.readInt();
 
-        for(int x = 1; x<= numero; x++) {
+        for(int x = 0; x<= numero; x++) {
             if( (x % 3 == 0) && (x % 7 == 0) ) {
                 System.out.println("Número "+x+" é múltiplo de 3 e 7");
             }
+        }
+        System.out.println("Aperte uma tecla para executar o próximo exercício");
+        System.in.read();
+
+        // segunda parte do exercicio
+        for(int x = 0; x<= 100; x++) {
+            if( x % 2 == 0 )
+                System.out.println(x+" é par");
+            else
+                System.out.println(x+" é ímpar");
+
         }
 
     }
